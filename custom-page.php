8778e1e75534f57105da3d2a8ba9c559634bc8fb
vivek865 <kumarvivek865@gmail.com>
2016-04-26 11:32:44 +0530

    Update custom-page.php

--- custom-page.php
+++ custom-page.php
@@ -13,3 +13,31 @@
 		}
 		wp_reset_postdata();
 	?>
+// for multiple category
+<?php 	$args = array('tax_query' => array(
+        'relation' => 'OR',
+        array(
+            'taxonomy' => 'product_cat',
+            'field' => 'slug',
+            'terms' => 'activity-trackers'
+        ),
+        array(
+            'taxonomy' => 'product_cat',
+            'field' => 'slug',
+            'terms' => 'sports-fitness'
+        )
+    ),
+    'post_type' => 'product',
+			'posts_per_page' => 12
+			);
+			$loop = new WP_Query( $args );
+		if ( $loop->have_posts() ) {
+			while ( $loop->have_posts() ) : $loop->the_post();
+				wc_get_template_part( 'content', 'product' );
+			endwhile;
+		} else {
+			echo __( 'No products found' );
+		}
+		wp_reset_postdata();
+	?>
+
