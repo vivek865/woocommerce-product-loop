b63ce4f5616a9d07ca96edb2d055371d192145b3
vivek865 <kumarvivek865@gmail.com>
2016-04-26 09:21:55 +0530

    custom loop for product
    just create  custom content product template located in woocommerce  template (generally we  copy the content_product to new file name it any name like content-product_custom.php changing the css according o theme requirement   ),  & replacing the code line no 9 	wc_get_template_part( 'content', 'product' ); with 	wc_get_template_part( 'content', 'product_custom' );

---
+++ custom-page.php
@@ -0,0 +1,15 @@
+<?php
+		$args = array(
+			'post_type' => 'product',
+			'posts_per_page' => 12
+			);
+		$loop = new WP_Query( $args );
+		if ( $loop->have_posts() ) {
+			while ( $loop->have_posts() ) : $loop->the_post();
+				wc_get_template_part( 'content', 'product' );
+			endwhile;
+		} else {
+			echo __( 'No products found' );
+		}
+		wp_reset_postdata();
+	?>
